af366b39a041090ef6bff87c0252008880a42581
lyla <demange.telecom@gmail.com>
2024-07-12 00:40:29 +0000

    [bug fix] dangle wiggles for walks AND swings

--- scenes/Weapon.gd
+++ scenes/Weapon.gd
@@ -12,13 +12,13 @@ func _ready():
 func _process(delta):
 	pass
 
-#called when the player wants to swing its stick -by pressing the S button when I'm writting this
-#The function has to move the stick but also deal with the cute movements of the dangle
+#called when the player wants to swing its stick -by pressing the S button for now
+#The swing function has to move the stick but also deal with the movements of the dangle
 func swing():
 	if swing_finished : #the player cannot spam-click the swing
 		swing_finished = false
 		if stickType == "crosse" :
-			$Dangle/animation.play("crosse_swing")
+			$Dangle/animation.play("crosse_swing") #WHY DOESN'T IT SWING ???
 		$Stick/animation.play("swing")
 
 
@@ -26,12 +26,20 @@ func _on_stick_animation_finished(anim_name):
 	if anim_name == "swing" :
 		swing_finished = true
 
-#handles the left/right deplacements of the player
+
+
+#handles the movements while the walk of the player
 func _on_player_idle():
-	$Dangle/animation.play("RESET")
+	if swing_finished :
+		$Dangle/animation.play("RESET")
 func _on_player_move_left():
 	if stickType == "crosse" :
 		$Dangle/animation.play("crosse_float_left")
 func _on_player_move_right():
 	if stickType == "crosse" :
 		$Dangle/animation.play("crosse_float_right")
+
+
+
+func _on_Dangle_animation_started(anim_name):
+	if anim_name != "RESET" : print(anim_name)
